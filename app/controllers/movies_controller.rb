575a8d99faa8bf41eb9469d4fd3be54a2608177c
tf-jlemasters <jlemasters@tracefirst.com>
2022-03-29 10:59:16 -0600

    added debounce js controller, refactored movie controller, updated views

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,7 +6,11 @@ class MoviesController < ApplicationController
   end
 
   def search
-    @movies = Movie.where('title LIKE ?', "%#{params[:title_search]}%")
+    if params[:title_search].present?
+      @movies = Movie.filter_by_title(params[:title_search])
+    else
+        @movies = []
+    end
     respond_to do |format|
         format.turbo_stream do
           render turbo_stream: turbo_stream.update("search_results", partial: "movies/search_results", locals: { movies: @movies })
